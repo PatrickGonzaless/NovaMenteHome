3de1a9c9dfb4cb8336432dac953056fbb5ad61a1
Patrick Gonzales <101566290+PatrickGonzaless@users.noreply.github.com>
2022-06-21 22:00:52 -0300

    Update Home.js

--- Home.js
+++ Home.js
@@ -1,4 +1,4 @@
-
+//fecha/abre
 
 function fecha_abre1(){
     
@@ -13,7 +13,7 @@ function fecha_abre2(){
     document.querySelector(".acess1-fake").style.visibility="hidden";
 }
 
-// --------------
+// Modo normal da página
 
 function normalmode(){
     document.querySelector("#corpo").style.backgroundImage="url(Imagens/background-home.png)"
@@ -64,13 +64,14 @@ function normalmode(){
     document.querySelector("#img7").src="Imagens/imghome7.png"
     document.querySelector("#img11").src="Imagens/imghome11.png"
     document.querySelector("#img12").src="Imagens/imghome12.png"
-    document.querySelector("#img13").src="Imagens/imghome13.png"
     document.querySelector("#sec_footer").style.backgroundColor="#9D6BFF"
     document.querySelector(".plataformah3").style.color="white"
     document.querySelector(".socialh3").style.color="white"
     document.querySelector(".contatoh3").style.color="white"
     document.querySelector(".team").style.background="linear-gradient(95.21deg, #8547FF 18.88%, #1F3A71 173.97%)"
 }
+
+//Modo Escuro da página
 
 function darkmode(){
     document.querySelector("#corpo").style.backgroundImage="url(Imagens/background-darkhome.png)"
@@ -121,11 +122,131 @@ function darkmode(){
     document.querySelector("#img7").src="Imagens/imghomedark7.png"
     document.querySelector("#img11").src="Imagens/imghomedark11.png"
     document.querySelector("#img12").src="Imagens/imghomedark12.png"
-    document.querySelector("#img13").src="Imagens/imghomedark13.png"
     document.querySelector("#sec_footer").style.backgroundColor="#363636"
     document.querySelector(".plataformah3").style.color="#737373"
     document.querySelector(".socialh3").style.color="#737373"
     document.querySelector(".contatoh3").style.color="#737373"
     document.querySelector(".team").style.background="linear-gradient(120.35deg, #323232 18.23%, #FFFFFF 308.93%, #1F3A71 310.79%)"
-    
 }
+
+//Baixa Visão
+
+    function baixavisao(){
+        document.querySelector("#nav_header").style.transform="scale(1.3)";
+
+        document.querySelector(".primeiraparte_titulo").style.transform="scale(1.5)";
+        document.querySelector(".primeiraparte_texto").style.transform="scale(1.5)";   
+
+        document.querySelector(".cadastrar").style.transform="scale(1.5)";
+        document.querySelector(".cadastrar").style.marginTop="10vh";
+
+        // document.querySelector(".divh3").style.transform="scale(1.5)";
+        // document.querySelector(".divh3_p").style.transform="scale(1.5)";
+        // document.querySelector(".divh3_2").style.transform="scale(1.5)";
+        // document.querySelector(".divh3_2 p").style.transform="scale(1.5)";
+        // document.querySelector(".divh3_3").style.transform="scale(1.5)";
+        // document.querySelector(".divh3_p").style.transform="scale(1.5)";
+
+        document.querySelector(".parte_roxa").style.fontSize="40px";
+        document.querySelector(".parte_roxa2").style.fontSize="40px";
+
+        document.querySelector(".roxo1").style.width="40vw";
+        document.querySelector(".roxo1").style.height="40vh";
+
+        document.querySelector(".roxo2").style.width="28vw";
+        document.querySelector(".roxo2").style.height="40vh";
+
+        document.querySelector(".roxo3").style.width="32vw";
+        document.querySelector(".roxo3").style.height="44vh";
+        document.querySelector(".roxo3").style.marginTop="30vh";
+
+        document.querySelector(".roxo_texto1").style.fontSize="25px";
+        document.querySelector(".roxo_texto1").style.width="35vw";
+
+        document.querySelector(".roxo_texto2").style.fontSize="25px";
+
+        document.querySelector(".roxo_texto3").style.fontSize="25px";
+        
+        document.querySelector(".maisinfo_titulo").style.fontSize="45px";
+        document.querySelector(".maisinfo_texto").style.fontSize="30px";
+
+        document.querySelector(".maisinfo_titulo2").style.fontSize="45px";
+        document.querySelector(".maisinfo_texto2").style.fontSize="30px";
+
+        document.querySelector(".maisinfo_titulo3").style.fontSize="45px";
+        document.querySelector(".maisinfo_texto3").style.fontSize="30px";
+
+        document.querySelector(".fundo_roxo").style.height="90vh";
+        document.querySelector(".fundo_roxo_titulo1").style.fontSize="35px";
+        document.querySelector(".fundo_roxo_texto1").style.fontSize="22px";
+        document.querySelector(".fundo_roxo_titulo2").style.fontSize="35px";
+        document.querySelector(".fundo_roxo_titulo2").style.paddingTop="0vh";
+        document.querySelector(".fundo_roxo_texto2").style.fontSize="22px";
+
+        document.querySelector(".opiniao_centro").style.fontSize="20px";
+
+        document.querySelector(".plataformah3").style.fontSize="35px";
+        document.querySelector(".navplataforma a").style.fontSize="25px";
+
+        document.querySelector(".socialh3").style.fontSize="35px";
+        document.querySelector(".contatoh3").style.fontSize="35px";
+    }
+
+    function normalvisao(){
+        document.querySelector("#nav_header").style.transform="scale(1.0)";
+
+        document.querySelector(".primeiraparte_titulo").style.transform="scale(1.0)";
+        document.querySelector(".primeiraparte_texto").style.transform="scale(1.0)";
+
+        document.querySelector(".cadastrar").style.transform="scale(1.0)";
+        document.querySelector(".cadastrar").style.marginTop="3vh";
+
+        // document.querySelector(".divh3").style.transform="scale(1.0)";
+        // document.querySelector(".divh3_p").style.transform="scale(1.0)";
+        // document.querySelector(".divh3_2").style.transform="scale(1.0)";
+        // document.querySelector(".divh3_2 p").style.transform="scale(1.0)";
+        // document.querySelector(".divh3_3").style.transform="scale(1.0)";
+
+        document.querySelector(".parte_roxa").style.fontSize="25px";
+        document.querySelector(".parte_roxa2").style.fontSize="25px";
+
+        document.querySelector(".roxo1").style.width="26vw";
+        document.querySelector(".roxo1").style.height="31vh";
+
+        document.querySelector(".roxo2").style.width="26vw";
+        document.querySelector(".roxo2").style.height="31vh";
+
+        document.querySelector(".roxo3").style.width="26vw";
+        document.querySelector(".roxo3").style.height="31vh";
+        document.querySelector(".roxo3").style.marginTop="22vh";
+
+        document.querySelector(".roxo_texto1").style.fontSize="15px";
+        document.querySelector(".roxo_texto1").style.width="19.5vw";
+
+        document.querySelector(".roxo_texto2").style.fontSize="15px";
+
+        document.querySelector(".roxo_texto3").style.fontSize="15px";
+
+        document.querySelector(".maisinfo_titulo").style.fontSize="30px";
+        document.querySelector(".maisinfo_texto").style.fontSize="17px";
+
+        document.querySelector(".maisinfo_titulo2").style.fontSize="30px";
+        document.querySelector(".maisinfo_texto2").style.fontSize="17px";
+
+        document.querySelector(".maisinfo_titulo3").style.fontSize="30px";
+        document.querySelector(".maisinfo_texto3").style.fontSize="17px";
+
+        document.querySelector(".fundo_roxo").style.height="90vh";
+        document.querySelector(".fundo_roxo_titulo1").style.fontSize="25px";
+        document.querySelector(".fundo_roxo_texto1").style.fontSize="20px";
+        document.querySelector(".fundo_roxo_titulo2").style.fontSize="25px";
+        document.querySelector(".fundo_roxo_titulo2").style.paddingTop="6vh";
+        document.querySelector(".fundo_roxo_texto2").style.fontSize="20px";
+
+        document.querySelector(".opiniao_centro").style.fontSize="15px";
+
+        document.querySelector(".plataformah3").style.fontSize="25px";
+        
+        document.querySelector(".socialh3").style.fontSize="25px";
+        document.querySelector(".contatoh3").style.fontSize="25px";
+    }
